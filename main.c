300dea78b3322797837d604bc057da6938cdbe66
Max Bernstein <tekknolagi@gmail.com>
2014-10-13 15:15:39 -0400

    remove other crap regarding while loops

--- main.c
+++ main.c
@@ -8,32 +8,22 @@
 
 #define p(x) puts(#x)
 
-volatile sig_atomic_t running = 1;
-
 void interrupt_handler (int sig) {
-  p(interrupted with\n);
-  printf("%d\n", sig);
-  running = 0;
   exit(5);
 }
 
 int main () {
   sock_t sock;
   if (!sock_init(&sock, 1024)) return 1;
-  p(init);
   if (!sock_bind(&sock, 15002)) return 2;
-  p(bound);
   if (!sock_listen(&sock)) return 3;
-  p(listening);
 
   signal(SIGINT, interrupt_handler);
 
-  while (running) {
+  while (true) {
     if (!sock_accept(&sock)) return 4;
-    p(accept);
 
     sock_recv(&sock);
-    printf("%s\n", sock.buffer);
     sock_write(&sock, "hello, world!\n\0");
     sock_close(&sock, 1);
   }
