7f3a09f5f729e5d9a564a3006d7968f0ae8d0573
Max Bernstein <tekknolagi@gmail.com>
2014-11-01 00:09:07 -0400

    what a sad state...

--- main.c
+++ main.c
@@ -17,6 +17,9 @@ void interrupt_handler (int sig) {
 char *cat (void *sock) {
   assert(sock != NULL);
 
+  // cast because otherwise we have this weird circular
+  // fustercluck in the header
+  // - too tired to fix right now
   ssock_t *s = (ssock_t *) sock;
   return s->buffer;
 }
