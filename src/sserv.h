406743df7272a298838c7727069d5cb98dff3e5d
Max Bernstein <tekknolagi@gmail.com>
2014-11-01 00:13:08 -0400

    no longer neeeeded

--- src/sserv.h
+++ src/sserv.h
@@ -12,10 +12,6 @@ typedef enum {
   SSERV_RESPONDF
 } sserv_status_t;
 
-typedef struct sserv_settings_s {
-  int port, bufsize, backlog;
-} sserv_settings_t;
-
 sserv_status_t sserv_init (ssock_t *);
 sserv_status_t sserv_serve (ssock_t *);
 
